3688577a14d42d25867770789e716039d4c37f36
Oomkar Pandit <oomkar@yahoo.com>
2018-08-12 15:42:51 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
 var playlist={'dion':'titanic'}
-function updatePlaylist(playlist,artistName,SongTitle){
-  playlist.ricky='livin-da-vica-loca'
+function updatePlaylist(playlist1,artistName,SongTitle){
+  playlist1.ricky='livin-da-vica-loca'
 }
